2af8eed47cf92a729bb8ea5d40120fb01c0d219d
Kasper Iversen <kasperi@hiof.no>
2023-01-24 22:39:08 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// change navbar on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 100)
+})
+
+// show / hide answer from the FAQs
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon 
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus"
+        } else {
+            icon.className = "uil uil-plus"
+        }
+    })
+})
